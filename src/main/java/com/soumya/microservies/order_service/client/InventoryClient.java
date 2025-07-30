27c0c623b4df5bddfa98060d2cda3c0756956864
soumya ranjan swain <soumyaranjanswain877@gmail.com>
2025-07-31 00:31:15 +0530

    Feature added (CircuitBreaker)

--- src/main/java/com/soumya/microservies/order_service/client/InventoryClient.java
+++ src/main/java/com/soumya/microservies/order_service/client/InventoryClient.java
@@ -1,13 +1,30 @@
 package com.soumya.microservies.order_service.client;
 
 
+import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
+import io.github.resilience4j.retry.annotation.Retry;
+//import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.service.annotation.GetExchange;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 //@FeignClient(name = "inventory", url = "${inventory.url}")
+
 public interface InventoryClient {
+
+    Logger log = LoggerFactory.getLogger(InventoryClient.class);
 
     //@RequestMapping(method = RequestMethod.GET, value = "/api/inventory")
     @GetExchange("/api/inventory")
+    @CircuitBreaker(name="inventory", fallbackMethod = "fallbackMethod")
+    @Retry(name="inventory")
     boolean isInStock(@RequestParam String skuCode, @RequestParam Integer quantity);
+
+    default boolean fallbackMethod(String skuCode, Integer quantity, Throwable throwable) {
+        // Log the error or handle it as needed
+        log.info("Fallback method called for skuCode: {}, quantity: {}, error: {}", skuCode, quantity, throwable.getMessage());
+        return false; // Default response when inventory service is unavailable
+    }
 }
--- src/main/java/com/soumya/microservies/order_service/config/RestClientConfig.java
+++ src/main/java/com/soumya/microservies/order_service/config/RestClientConfig.java
@@ -2,11 +2,17 @@ package com.soumya.microservies.order_service.config;
 
 import com.soumya.microservies.order_service.client.InventoryClient;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.http.client.ClientHttpRequestFactorySettings;
+import org.springframework.boot.web.client.ClientHttpRequestFactories;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.client.ClientHttpRequestFactory;
+import org.springframework.http.client.SimpleClientHttpRequestFactory;
 import org.springframework.web.client.RestClient;
 import org.springframework.web.client.support.RestClientAdapter;
 import org.springframework.web.service.invoker.HttpServiceProxyFactory;
+
+import java.time.Duration;
 
 @Configuration
 public class RestClientConfig {
@@ -18,11 +24,20 @@ public class RestClientConfig {
     public InventoryClient inventoryClient() {
         RestClient restClient = RestClient.builder()
                 .baseUrl(inventoryServiceUrl)
+                .requestFactory(getClientRequestFactory())
                 .build();
         var restClientAdapter = RestClientAdapter.create(restClient);
         var httpServiceProxyFactory = HttpServiceProxyFactory.builderFor(restClientAdapter).build();
 
 
         return httpServiceProxyFactory.createClient(InventoryClient.class);
+    }
+
+    private ClientHttpRequestFactory getClientRequestFactory() {
+        // You can customize the ClientHttpRequestFactory here if needed
+        SimpleClientHttpRequestFactory settings = new SimpleClientHttpRequestFactory();
+        settings.setConnectTimeout(3000); // 3 seconds
+        settings.setReadTimeout(3000);
+        return settings;
     }
 }
